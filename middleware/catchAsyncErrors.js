bea05a33b04269f6dcc374ec66bce72e9b06c871
Rupyez <rupeshduwal17@gmail.com>
2023-02-15 13:41:09 +0545

    again commit

--- middleware/catchAsyncErrors.js
+++ middleware/catchAsyncErrors.js
@@ -1,4 +1,13 @@
+// module.exports = (theFunc) => (req,res,next) =>{
+//     Promise.resolve(theFunc(req,res,next)).catch(next);
+// }
 
-module.exports = (theFunc) => (req,res,next) =>{
-    Promise.resolve(theFunc(req,res,next)).catch(next);
-}
+let catchAsyncErrors = (action) => async (req, res, next) => {
+  try {
+    await action(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = catchAsyncErrors;
