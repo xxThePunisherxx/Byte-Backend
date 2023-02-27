801246e11c84095a5926936260476cd7a376961c
Rupyez <rupeshduwal17@gmail.com>
2023-02-27 14:01:12 +0545

    final commit

--- models/Partner.js
+++ models/Partner.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
-const partnerSchema = new mongoose.Schema({
-  // schema
-  companyWebsite: {
-    type: String,
-    required: true,
-  },
+const partnerSchema = new mongoose.Schema(
+  {
+    // schema
+    companyWebsite: {
+      type: String,
+      required: true,
+    },
 
-  image: {
-    type: String,
-    required: true,
+    image: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Partner = new mongoose.model("Partner", partnerSchema);
 module.exports = Partner;
--- models/RegisterForm.js
+++ models/RegisterForm.js
@@ -1,31 +1,37 @@
 const mongoose = require("mongoose");
 
-const registerFormSchema = new mongoose.Schema({
-  // schema
-  legalName: {
-    type: String,
-    required: true,
-  },
+const registerFormSchema = new mongoose.Schema(
+  {
+    // schema
+    legalName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-  email: {
-    type: String,
-    required: true,
-  },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-  academicLevel: {
-    type: String,
-    required: true,
-  },
+    academicLevel: {
+      type: String,
+      required: true,
+    },
 
-  phoneNumber: {
-    type: Number,
-    required: true,
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    course: {
+      type: String,
+      required: true,
+    },
   },
-  course: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const RegisterForm = new mongoose.model("RegisterForm", registerFormSchema);
 module.exports = RegisterForm;
