75a30deb6a46752ee370298077a8aba1f0a806bd
Nikesh <nikeshkhatri763@gmail.com>
2023-03-04 09:20:23 +0545

    Fix: Backend Error

--- controllers/createFile.js
+++ controllers/createFile.js
@@ -1,42 +1,41 @@
-const port =  require("../config/config.js");
-
+const port = require("../config/config.js");
 
 const createFile = (req, res) => {
-  //for single file there is req.file in {}
-  //for multiple file there is req.files in [{},{}]
-  // req.file will be undefined fro req.multiple
+	//for single file there is req.file in {}
+	//for multiple file there is req.files in [{},{}]
+	// req.file will be undefined fro req.multiple
 
-  console.log("**********", req.file);
-  if (req.file) {
-    //   ************for single file
-    // here file information is given in req.file
-    //where as other text information is given in req.body
-    // let pathArray = req.file.path.split("\\"); //vvimp to split with \  use \\
-    let fileName = req.file.filename;
-    // let path = { path: `http://localhost:${port}/${fileName}` };
-    let path = { path: `https://learning-management-system-kx6y.onrender.com/${fileName}` };
+	console.log("**********", req.file);
+	if (req.file) {
+		//   ************for single file
+		// here file information is given in req.file
+		//where as other text information is given in req.body
+		// let pathArray = req.file.path.split("\\"); //vvimp to split with \  use \\
+		let fileName = req.file.filename;
+		// let path = { path: `http://localhost:${port}/${fileName}` };
+		let path = { path: `https://byte-backend-demo.up.railway.app/${fileName}` };
 
-    let successJson = {
-      status: "success",
-      message: "File uploaded successfully.",
-      path: path,
-    };
-    res.json(successJson);
-    // if mainhost/.file    is added it is searched in static file
-    //localhost:8000/1672933159169houseOfJobBannerImage.png
-  } else {
-    let paths = req.files.map((file) => {
-      let fileName = file.filename;
-      let path = `http://localhost:${port}/${fileName}`;
-      return { path: path };
-    });
-    let successJson = {
-      status: "success",
-      message: "Files uploaded successfully.",
-      paths: paths,
-    };
-    res.json(successJson);
-  }
+		let successJson = {
+			status: "success",
+			message: "File uploaded successfully.",
+			path: path,
+		};
+		res.json(successJson);
+		// if mainhost/.file    is added it is searched in static file
+		//localhost:8000/1672933159169houseOfJobBannerImage.png
+	} else {
+		let paths = req.files.map((file) => {
+			let fileName = file.filename;
+			let path = `http://localhost:${port}/${fileName}`;
+			return { path: path };
+		});
+		let successJson = {
+			status: "success",
+			message: "Files uploaded successfully.",
+			paths: paths,
+		};
+		res.json(successJson);
+	}
 };
 
-module.exports = createFile
+module.exports = createFile;
